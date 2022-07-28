3aebdcf26c9450968958fb58459888d39be50160
Space-Belt <dngurwn@gmail.com>
2022-07-28 22:59:36 +0900

    [Init] 프로젝트 초기화

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Heart-Lab-Chat</title>
+      </Head>
+      <div>aaa</div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
