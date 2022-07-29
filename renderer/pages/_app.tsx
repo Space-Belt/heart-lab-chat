955bfce6f53967b0ca7ceae4ac7ec85cb99d08db
Space-Belt <dngurwn@gmail.com>
2022-07-29 14:45:20 +0900

    [Feat] Context API 사용 로그인 회원가입 나누기위함

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import "../styles/global.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { LJProvider } from "../context/LoginJoin";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
+    <LJProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>채팅앱</title>
       </Head>
       <Component {...pageProps} />
-    </React.Fragment>
+    </LJProvider>
   );
 }
 
